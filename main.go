b161c7598fc81e2eec7e7bb319a063594f4c340a
shumon84 <cameremon84@gmail.com>
2019-05-29 12:26:55 +0900

    リファクタリング

--- main.go
+++ main.go
@@ -5,23 +5,44 @@ import (
 	"time"
 )
 
-func FirstWeekDay(year int) int {
+func firstWeekDay(year int) int {
 	weekday := time.Date(year, 0, 0, 0, 0, 0, 0, time.Local).Weekday()
 	return int(weekday)
 }
 
-func main() {
-	year, week := time.Now().ISOWeek()
-	week = 1
+func GetDayOfWeek(date time.Time, weekday time.Weekday) time.Time {
+	year, week := date.ISOWeek()
 	thisWeekSunday := time.Date(year, 1, 1, 0, 0, 0, 0, time.Local).
 		AddDate(0, 0, (week-1)*7).
-		AddDate(0, 0, FirstWeekDay(year))
-	thisWeekMonday := thisWeekSunday.AddDate(0, 0, -6)
-	thisWeekFriday := thisWeekSunday.AddDate(0, 0, -2)
+		AddDate(0, 0, firstWeekDay(year))
+	offsetDate := 0
+	switch weekday {
+	case time.Monday:
+		offsetDate = -6
+	case time.Tuesday:
+		offsetDate = -5
+	case time.Wednesday:
+		offsetDate = -4
+	case time.Thursday:
+		offsetDate = -3
+	case time.Friday:
+		offsetDate = -2
+	case time.Saturday:
+		offsetDate = -1
+	case time.Sunday:
+		offsetDate = 0
+	}
+
+	return thisWeekSunday.AddDate(0, 0, offsetDate)
+}
+
+func main() {
+	monday := GetDayOfWeek(time.Now(), time.Monday)
+	friday := GetDayOfWeek(time.Now(), time.Friday)
 	fmt.Printf("週報(%s ~ %s)\n"+
 		"# やったこと\n"+
 		"# 業務に関係ないやったこと\n",
-		thisWeekMonday.Format("2006/01/02"),
-		thisWeekFriday.Format("02"),
+		monday.Format("2006/01/02"),
+		friday.Format("02"),
 	)
 }
