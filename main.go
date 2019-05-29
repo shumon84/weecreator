9386281fba3ffb6d3a052a596a9c13d66449b98d
shumon84 <cameremon84@gmail.com>
2019-05-29 12:05:27 +0900

    週報の雛形を自動生成できるやつを作った

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func FirstWeekDay(year int) int {
+	weekday := time.Date(year, 0, 0, 0, 0, 0, 0, time.Local).Weekday()
+	return int(weekday)
+}
+
+func main() {
+	year, week := time.Now().ISOWeek()
+	week = 1
+	thisWeekSunday := time.Date(year, 1, 1, 0, 0, 0, 0, time.Local).
+		AddDate(0, 0, (week-1)*7).
+		AddDate(0, 0, FirstWeekDay(year))
+	thisWeekMonday := thisWeekSunday.AddDate(0, 0, -6)
+	thisWeekFriday := thisWeekSunday.AddDate(0, 0, -2)
+	fmt.Printf("週報(%s ~ %s)\n"+
+		"# やったこと\n"+
+		"# 業務に関係ないやったこと\n",
+		thisWeekMonday.Format("2006/01/02"),
+		thisWeekFriday.Format("02"),
+	)
+}
